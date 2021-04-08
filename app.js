4b63e8b056d7cd80f82ba7b9e1f89019bfedecee
MD Jakaria <72543122+CoderJakaria@users.noreply.github.com>
2021-04-08 05:34:06 -0700

    Upload Changes

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const equalTo = document.querySelector(".oper");
+
+
+    
+     const inputValue = document.querySelector("#display_answer");
+     if(inputValue.value=="undefined"){
+          inputValue.innerText="Iam"
+     }
+
+
+
